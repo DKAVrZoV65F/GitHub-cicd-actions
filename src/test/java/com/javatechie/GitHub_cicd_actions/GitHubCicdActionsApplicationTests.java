bbcb9d3620d29b75cc392cd592d90eb870a5cb54
DKAVrZoV65F <gw9ckwfsp@mozmail.com>
2025-01-28 20:39:26 +0300

    feat: added CI/CD-Demo-Project

---
+++ src/test/java/com/javatechie/GitHub_cicd_actions/GitHubCicdActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.GitHub_cicd_actions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubCicdActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
